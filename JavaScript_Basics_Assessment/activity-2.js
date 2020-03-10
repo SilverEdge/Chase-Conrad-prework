f0c13f306718b73a46859020eee234d49732f719
SilverEdge <chase@chaseconrad.com>
2020-03-10 18:18:55 -0500

    completed activities 2, 3, and 4

---
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -0,0 +1,23 @@
+// The product of 9 and 9
+let product = 9 * 9;
+console.log(product);
+
+// The quotient of 56 and 12
+let quotient = 56 / 12;
+console.log(quotient);
+
+// The remainder of 281 divided by 9
+let remainder = 281 % 9;
+console.log(remainder);
+
+// 18 divided by 0
+let divide0 = 18 / 0;
+console.log(divide0);
+
+// The sum of string 56 and string 92
+let sum = '56' + '92';
+console.log(sum);
+
+// Using any comparison operator you wish on the numbers 42 and 52, make it evaluate to false.
+let bool = 42 == 52;
+console.log(bool);
---
+++ JavaScript_Basics_Assessment/activity-3.js
@@ -0,0 +1,16 @@
+// Grab a user input using a prompt() and store it in a variable.
+
+var name = prompt("Please enter your name.");
+
+// Add a conditional statement where if the variable's length is greater than 4,
+// we alert the user that their name is greater than four characters.
+
+if (name.length > 4) {
+    alert("Your name is greater than four characters!");
+}
+
+// Otherwise, alert that their name is less than four characters.
+
+else{
+    alert("Your name is not greater than four characters!");
+};
